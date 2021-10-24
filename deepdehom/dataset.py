1aaf3e288b58358a2eb940739b67967a633471a6
Martin Elingaard <martinelingaard@MacBook-Pro-tilhrende-Martin.local>
2021-10-24 17:18:05 +0200

    initial pytorch-lightning commit

--- deepdehom/dataset.py
+++ deepdehom/dataset.py
@@ -1,13 +1,9 @@
 import os
-import math
 from typing import Tuple
 
 import numpy as np
 import torch
-from torch.utils.data.sampler import BatchSampler, Sampler
 import torchvision.transforms.functional as TF
-from collections import defaultdict
-from itertools import repeat, chain
 
 class GaussRBF:
     """Simple class representation of Gaussian radial basis function
@@ -124,94 +120,6 @@ class RBFCollection:
 
         return input_field_recon
 
-class GroupedBatchSampler(BatchSampler):
-    """
-    Reference: https://github.com/pytorch/vision/blob/master/references/detection/group_by_aspect_ratio.py
-
-    Wraps another sampler to yield a mini-batch of indices.
-    It enforces that the batch only contain elements from the same group.
-    It also tries to provide mini-batches which follows an ordering which is
-    as close as possible to the ordering from the original sampler.
-    Arguments:
-        sampler (Sampler): Base sampler.
-        group_ids (list[int]): If the sampler produces indices in range [0, N),
-            `group_ids` must be a list of `N` ints which contains the group id of each sample.
-            The group ids must be a continuous set of integers starting from
-            0, i.e. they must be in the range [0, num_groups).
-        batch_size (int): Size of mini-batch.
-    """
-    def __init__(self, sampler, group_ids, batch_size):
-        if not isinstance(sampler, Sampler):
-            raise ValueError(
-                "sampler should be an instance of "
-                "torch.utils.data.Sampler, but got sampler={}".format(sampler)
-            )
-        self.sampler = sampler
-        self.group_ids = group_ids
-        self.batch_size = batch_size
-
-    def __iter__(self):
-        buffer_per_group = defaultdict(list)
-        samples_per_group = defaultdict(list)
-
-        num_batches = 0
-        for idx in self.sampler:
-            group_id = self.group_ids[idx]
-            buffer_per_group[group_id].append(idx)
-            samples_per_group[group_id].append(idx)
-            if len(buffer_per_group[group_id]) == self.batch_size:
-                yield buffer_per_group[group_id]
-                num_batches += 1
-                del buffer_per_group[group_id]
-            assert len(buffer_per_group[group_id]) < self.batch_size
-
-        # now we have run out of elements that satisfy
-        # the group criteria, let's return the remaining
-        # elements so that the size of the sampler is
-        # deterministic
-        expected_num_batches = len(self)
-        num_remaining = expected_num_batches - num_batches
-        if num_remaining > 0:
-            # for the remaining batches, take first the buffers with largest number
-            # of elements
-            for group_id, _ in sorted(buffer_per_group.items(),
-                                      key=lambda x: len(x[1]), reverse=True):
-                remaining = self.batch_size - len(buffer_per_group[group_id])
-                samples_from_group_id = _repeat_to_at_least(samples_per_group[group_id], remaining)
-                buffer_per_group[group_id].extend(samples_from_group_id[:remaining])
-                assert len(buffer_per_group[group_id]) == self.batch_size
-                yield buffer_per_group[group_id]
-                num_remaining -= 1
-                if num_remaining == 0:
-                    break
-        assert num_remaining == 0
-
-    def __len__(self):
-        return len(self.sampler) // self.batch_size
-
-def _repeat_to_at_least(iterable, n):
-    """Utility function used in the GroupedBatchSampler"""
-    repeat_times = math.ceil(n / len(iterable))
-    repeated = chain.from_iterable(repeat(iterable, repeat_times))
-    return list(repeated)
-
-def collate_fn(batch):
-    return tuple(zip(*batch))
-
-def get_group_ids(datapath):
-    """Loop through each sample in the specified directory and determine the group ID based on
-    the aspect ratio"""
-    filenames = os.listdir(datapath)
-    AR_list = []
-    for fname in filenames:
-        E_arr = np.load(os.path.join(datapath,fname))
-        h,w = E_arr[0].shape
-        AR_list.append(h/w)
-    # convert each unique aspect-ratio to a group id
-    enum_dict = {v: k for k, v in enumerate(set(AR_list))}
-    group_ids = [enum_dict[n] for n in AR_list]
-    return np.array(group_ids)
-
 class HomogenizationData(torch.utils.data.Dataset):
     """Dataset class used to handle homogenization data. Loads homogenization data from .npy files
     and applies RBF-encoding and optional data augmentation (horizontal and vertical flip). A value can be specified
@@ -265,7 +173,7 @@ class HomogenizationData(torch.utils.data.Dataset):
         H_rbf = self.rbf_encoding(ex,ey).astype(np.float32)
         # apply data augmentation
         H_rbf,ex,ey = self.transform(H_rbf,ex,ey)
-        return (H_rbf,ex,ey)
+        return H_rbf,ex,ey
 
     def rbf_encoding(self,ex:np.ndarray,ey:np.ndarray)->np.ndarray:
         """Function used to apply RBF encoding"""
